3c60ffabf2c3e70d90040430a45a86f6b9dcff7d
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-05-04 10:58:11 +0700

    create animation shimmer

---
+++ lib/src/widgets/animation/shimmer.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import '../../themes/colors.dart';
+
+class Shimmer extends StatelessWidget {
+  const Shimmer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Animate(
+      onPlay: (controller) => controller.loop(),
+      effects: const [
+        ShimmerEffect(
+          curve: Curves.easeOutCubic,
+          // color: Colors.purple,
+          colors: [PColors.loading, Colors.white, PColors.loading],
+          duration: Duration(seconds: 2),
+        )
+      ],
+      child: child,
+    );
+  }
+}
