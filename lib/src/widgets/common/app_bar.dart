4b87fb2da5a732d566390b7ce587c7950bcfba40
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-05-04 10:57:42 +0700

    create app bar widget

---
+++ lib/src/widgets/common/app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../constaints/ui_paramester.dart';
+
+class PAppBar extends StatelessWidget implements PreferredSizeWidget {
+
+  const PAppBar({super.key, this.leading, this.title, this.actions});
+
+  final Widget? leading;
+  final Widget? title;
+  final List<Widget>? actions;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: title,
+      actions: actions,
+      automaticallyImplyLeading: leading == null,
+      leading: leading,
+    );
+  }
+   @override
+  Size get preferredSize => UiParameters.appBarSize;
+}
