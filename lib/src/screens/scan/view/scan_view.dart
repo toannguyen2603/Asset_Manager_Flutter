67175be542e39879464003cd7d30df94651f9677
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-05-12 00:03:02 +0700

    #Scanner: Add mobile scanner using MLKit

--- lib/src/screens/scan/view/scan_view.dart
+++ lib/src/screens/scan/view/scan_view.dart
@@ -1,12 +1,162 @@
-import 'package:flutter/cupertino.dart';
+import 'package:asset_manager_flutter/src/constaints/app_sizes.dart';
+import 'package:asset_manager_flutter/src/themes/colors.dart';
+import 'package:asset_manager_flutter/src/themes/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-import '../../../constaints/app_sizes.dart';
-
-class ScannerView extends StatelessWidget {
+class ScannerView extends StatefulWidget {
   const ScannerView({super.key});
 
   @override
+  State<ScannerView> createState() => _ScannerViewState();
+}
+
+class _ScannerViewState extends State<ScannerView>
+    with SingleTickerProviderStateMixin {
+  BarcodeCapture? barcode;
+
+  MobileScannerController controller = MobileScannerController();
+
+  bool isStarted = true;
+  double _zoomFactor = 0.0;
+
+  @override
   Widget build(BuildContext context) {
-    return Column();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Scanner',
+          style: PStyle.poppins(context)!.copyWith(
+            color: AColors.textColor,
+            fontSize: Sizes.p16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        elevation: 0,
+      ),
+      body: SafeArea(
+          child: Container(
+        color: AColors.primaryColor,
+        child: Stack(children: [
+          MobileScanner(
+            controller: controller,
+            scanWindow: Rect.fromCenter(
+                center: Offset(10, 10), width: 100.0, height: 100.0),
+            onDetect: (barcode) {
+              setState(() {
+                this.barcode = barcode;
+              });
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Colors.black.withOpacity(0.4),
+              height: 100,
+              child: Column(
+                children: [
+                  Slider(
+                    value: _zoomFactor,
+                    onChanged: (value) {
+                      setState(() {
+                        _zoomFactor = value;
+                        controller.setZoomScale(value);
+                      });
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        color: Colors.white,
+                        icon: ValueListenableBuilder(
+                          valueListenable: controller.torchState,
+                          builder: (context, state, child) {
+                            if (state == null) {
+                              return const Icon(
+                                Icons.flash_off,
+                                color: Colors.grey,
+                              );
+                            }
+                            switch (state) {
+                              case TorchState.off:
+                                return const Icon(
+                                  Icons.flash_off,
+                                  color: Colors.grey,
+                                );
+                              case TorchState.on:
+                                return const Icon(
+                                  Icons.flash_on,
+                                  color: Colors.yellow,
+                                );
+                            }
+                          },
+                        ),
+                        iconSize: 32.0,
+                        onPressed: () => controller.toggleTorch(),
+                      ),
+                      IconButton(
+                        color: Colors.white,
+                        icon: ValueListenableBuilder(
+                          valueListenable: controller.cameraFacingState,
+                          builder: (context, state, child) {
+                            if (state == null) {
+                              return const Icon(Icons.camera_front);
+                            }
+                            switch (state as CameraFacing) {
+                              case CameraFacing.front:
+                                return const Icon(Icons.camera_front);
+                              case CameraFacing.back:
+                                return const Icon(Icons.camera_rear);
+                            }
+                          },
+                        ),
+                        iconSize: 32.0,
+                        onPressed: () => controller.switchCamera(),
+                      ),
+                      IconButton(
+                        color: Colors.white,
+                        icon: const Icon(Icons.image),
+                        iconSize: 32.0,
+                        onPressed: () async {
+                          final ImagePicker picker = ImagePicker();
+                          // Pick an image
+                          final XFile? image = await picker.pickImage(
+                            source: ImageSource.gallery,
+                          );
+                          if (image != null) {
+                            if (await controller.analyzeImage(image.path)) {
+                              if (!mounted) return;
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Barcode found!'),
+                                  backgroundColor: Colors.green,
+                                ),
+                              );
+                            } else {
+                              if (!mounted) return;
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('No barcode found!'),
+                                  backgroundColor: Colors.red,
+                                ),
+                              );
+                            }
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ]),
+      )),
+    );
   }
 }
