8356ac8e1fd286ee7dd066158c01390050e021b1
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-05-20 17:29:19 +0700

    #Fix: add filed

--- lib/src/screens/authentication/model/auth_user.dart
+++ lib/src/screens/authentication/model/auth_user.dart
@@ -6,9 +6,18 @@ part 'auth_user.g.dart';
 @freezed
 class LAuthUser with _$LAuthUser {
   factory LAuthUser(
-      {required String userName,
-      required String password,
-      String? email}) = _LAuthUser;
+      {String? id,
+      String? userName,
+      String? email,
+      String? phoneNumber,
+      @Default(false) bool isActive,
+      @Default(false) bool firstLogin,
+      String? token,
+      String? createDay,
+      String? updateDay,
+      String? role,
+      @Default(true) bool error,
+      @Default('') String message}) = _LAuthUser;
 
   factory LAuthUser.fromJson(Map<String, dynamic> json, {String}) =>
       _$LAuthUserFromJson(json);
